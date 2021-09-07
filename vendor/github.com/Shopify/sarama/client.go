e67956dcf2b79e342c5a8324aad7c9cb00d7d156
danielqsj <qsj.daniel@gmail.com>
2021-09-07 19:38:23 +0800

    update vendor

--- vendor/github.com/Shopify/sarama/client.go
+++ vendor/github.com/Shopify/sarama/client.go
@@ -475,7 +475,7 @@ func (client *client) RefreshMetadata(topics ...string) error {
 	// error. This handles the case by returning an error instead of sending it
 	// off to Kafka. See: https://github.com/Shopify/sarama/pull/38#issuecomment-26362310
 	for _, topic := range topics {
-		if len(topic) == 0 {
+		if topic == "" {
 			return ErrInvalidTopic // this is the error that 0.8.2 and later correctly return
 		}
 	}
@@ -493,7 +493,6 @@ func (client *client) GetOffset(topic string, partitionID int32, time int64) (in
 	}
 
 	offset, err := client.getOffset(topic, partitionID, time)
-
 	if err != nil {
 		if err := client.RefreshMetadata(topic); err != nil {
 			return -1, err
@@ -607,7 +606,7 @@ func (client *client) randomizeSeedBrokers(addrs []string) {
 }
 
 func (client *client) updateBroker(brokers []*Broker) {
-	var currentBroker = make(map[int32]*Broker, len(brokers))
+	currentBroker := make(map[int32]*Broker, len(brokers))
 
 	for _, broker := range brokers {
 		currentBroker[broker.ID()] = broker
@@ -892,7 +891,7 @@ func (client *client) tryRefreshMetadata(topics []string, attemptsRemaining int,
 			req.Version = 1
 		}
 		response, err := broker.GetMetadata(req)
-		switch err.(type) {
+		switch err := err.(type) {
 		case nil:
 			allKnownMetaData := len(topics) == 0
 			// valid response, use it
@@ -909,12 +908,12 @@ func (client *client) tryRefreshMetadata(topics []string, attemptsRemaining int,
 
 		case KError:
 			// if SASL auth error return as this _should_ be a non retryable err for all brokers
-			if err.(KError) == ErrSASLAuthenticationFailed {
+			if err == ErrSASLAuthenticationFailed {
 				Logger.Println("client/metadata failed SASL authentication")
 				return err
 			}
 
-			if err.(KError) == ErrTopicAuthorizationFailed {
+			if err == ErrTopicAuthorizationFailed {
 				Logger.Println("client is not authorized to access this topic. The topics were: ", topics)
 				return err
 			}
@@ -1053,7 +1052,6 @@ func (client *client) getConsumerMetadata(consumerGroup string, attemptsRemainin
 		request.CoordinatorType = CoordinatorGroup
 
 		response, err := broker.FindCoordinator(request)
-
 		if err != nil {
 			Logger.Printf("client/coordinator request to broker %s failed: %s\n", broker.Addr(), err)
 
