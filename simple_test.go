b90f9888dfe14373c28fd45a04e655645b42eba5
danielqsj <qsj.daniel@gmail.com>
2023-05-23 10:12:56 +0800

    fix test

--- simple_test.go
+++ simple_test.go
@@ -69,5 +69,5 @@ func runServer() {
 	opts.uriZookeeper = []string{"localhost:2181"}
 	opts.kafkaVersion = sarama.V1_0_0_0.String()
 	opts.metadataRefreshInterval = "30s"
-	setup("localhost:9304", "/metrics", ".*", ".*", false, opts, nil)
+	setup("localhost:9304", "/metrics", ".*", "^$", ".*", "^$", false, opts, nil)
 }
