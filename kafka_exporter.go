danielqsj/kafka_exporter

commit 9df9db8508b54aec8cab5ef75900bf5ad28291b6

- bump go 1.23 - all direct and transitive deps to latest ver - introduce gosec and staticcheck in makefile - gitignore ./vendor and go.sum as fetched externally - update code to reflect the updates of version and sec/static scan (partially)

kafka_exporter.go
@@ -5,7 +5,6 @@ import (
 	"crypto/x509"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -15,14 +14,16 @@ import (
 	"sync"
 	"time"
 
-	"github.com/Shopify/sarama"
+	"github.com/IBM/sarama"
 	kingpin "github.com/alecthomas/kingpin/v2"
 	"github.com/krallistic/kazoo-go"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	plog "github.com/prometheus/common/promlog"
 	plogflag "github.com/prometheus/common/promlog/flag"
+
 	"github.com/prometheus/common/version"
 	"github.com/rcrowley/go-metrics"
 	"k8s.io/klog/v2"
@@ -122,15 +123,15 @@ func CanReadCertAndKey(certPath, keyPath string) (bool, error) {
 	certReadable := canReadFile(certPath)
 	keyReadable := canReadFile(keyPath)
 
-	if certReadable == false && keyReadable == false {
+	if !certReadable && !keyReadable {
 		return false, nil
 	}
 
-	if certReadable == false {
+	if !certReadable {
 		return false, fmt.Errorf("error reading %s, certificate and key must be supplied as a pair", certPath)
 	}
 
-	if keyReadable == false {
+	if !keyReadable {
 		return false, fmt.Errorf("error reading %s, certificate and key must be supplied as a pair", keyPath)
 	}
 
@@ -216,7 +217,7 @@ func NewExporter(opts kafkaOpts, topicFilter string, topicExclude string, groupF
 		}
 
 		if opts.tlsCAFile != "" {
-			if ca, err := ioutil.ReadFile(opts.tlsCAFile); err == nil {
+			if ca, err := os.ReadFile(opts.tlsCAFile); err == nil {
 				config.Net.TLS.Config.RootCAs = x509.NewCertPool()
 				config.Net.TLS.Config.RootCAs.AppendCertsFromPEM(ca)
 			} else {
@@ -687,7 +688,7 @@ func (e *Exporter) collect(ch chan<- prometheus.Metric) {
 
 func init() {
 	metrics.UseNilMetrics = true
-	prometheus.MustRegister(version.NewCollector("kafka_exporter"))
+	prometheus.MustRegister(collectors.NewBuildInfoCollector())
 }
 
 //func toFlag(name string, help string) *kingpin.FlagClause {
@@ -957,7 +958,7 @@ func setup(
 
 		certPool := x509.NewCertPool()
 		if opts.serverTlsCAFile != "" {
-			if caCert, err := ioutil.ReadFile(opts.serverTlsCAFile); err == nil {
+			if caCert, err := os.ReadFile(opts.serverTlsCAFile); err == nil {
 				certPool.AppendCertsFromPEM(caCert)
 			} else {
 				klog.Error("error reading server ca")

commit 6bc6c414aa9055f7348e7d5da7b871cb4bb501a0

fix: incorrect build_info metric introduced in 9df9db85

kafka_exporter.go
@@ -21,11 +21,11 @@ import (
 	"github.com/krallistic/kazoo-go"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	plog "github.com/prometheus/common/promlog"
 	plogflag "github.com/prometheus/common/promlog/flag"
 
+	versionCollector "github.com/prometheus/client_golang/prometheus/collectors/version"
 	"github.com/prometheus/common/version"
 	"github.com/rcrowley/go-metrics"
 	"k8s.io/klog/v2"
@@ -707,7 +707,7 @@ func (e *Exporter) collect(ch chan<- prometheus.Metric) {
 
 func init() {
 	metrics.UseNilMetrics = true
-	prometheus.MustRegister(collectors.NewBuildInfoCollector())
+	prometheus.MustRegister(versionCollector.NewCollector("kafka_exporter"))
 }
 
 //func toFlag(name string, help string) *kingpin.FlagClause {